2ddde0c0d410340cf3ff32371fc81f043a0b2d46
Riki Fridrich <riki@fczbkk.com>
2012-05-25 15:47:34 +0200

    PreferenceObserver

---
+++ js/preference-observer.js
@@ -0,0 +1,36 @@
+﻿// TODO documentation and comments
+
+PreferenceObserver = function (callback, branch) {
+
+	this.callback = callback || function () {};
+	
+	var component = Components.classes['@mozilla.org/preferences-service;1'].getService(Components.interfaces.nsIPrefService);
+	if (branch) {component = component.getBranch(branch);}
+	this.component = component;
+	
+	this.register = function () {
+		this.component.QueryInterface(Components.interfaces.nsIPrefBranch2);
+		this.component.addObserver('', this, false);
+	};
+	
+	this.unregister = function () {
+		try {
+			this.component.removeObserver('', this);
+		} catch (err) {}
+	};
+	
+	this.observe = function (subject, topic, data) {
+		this.callback(data);
+	};
+	
+	this.register();
+	window.addEventListener('unload', this.unregister.bind(this));
+	
+}
+
+var myTest = new PreferenceObserver(
+	function (data) {
+		alert(data);
+	},
+	'extensions.myAddon'
+);
