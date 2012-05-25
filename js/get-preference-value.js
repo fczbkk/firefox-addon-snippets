720a43d1c1451f57a2bad6c9513383fe5fe1b7a4
Riki Fridrich <riki@fczbkk.com>
2012-05-25 11:06:50 +0200

    getPreferenceValue

---
+++ js/get-preference-value.js
@@ -0,0 +1,27 @@
+﻿/*
+When working with preferences in Firefox Addons, you need to call various
+methods, depending on the type of preference: boolean, string, integer.
+With this function, you can forget about value types, when you just want to
+get the damn value!
+*/
+
+function getPreferenceValue(pref) {
+	var component = Components.classes['@mozilla.org/preferences-service;1'].getService(Components.interfaces.nsIPrefService);
+	var prefType = component.getPrefType(pref);
+	var value = null;
+	switch (prefType) {
+		case component.PREF_BOOL:
+			value = component.getBoolPref(pref);
+			break;
+		case component.PREF_STRING:
+			value = component.getCharPref(pref);
+			break;
+		case component.PREF_INT:
+			value = component.getIntPref(pref);
+			break;
+		case component.PREF_INVALID:
+		default:
+			break;
+	}
+	return value;
+}
--- readme.txt
+++ readme.txt
@@ -1,3 +1,3 @@
 ﻿Firefox Addon Snippets
 
-This repo contains small snippets of code that will make your life working on Firefox Addons easier.
+This repo contains small snippets of code that will make your life working on Firefox Addons easier. Read the comments in the code, you might learn something.
