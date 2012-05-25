970ace57f741d86ce300f33593eeb16610142571
Riki Fridrich <riki@fczbkk.com>
2012-05-25 10:43:19 +0200

    isPrivateMode

---
+++ is-private-mode.js
@@ -0,0 +1,6 @@
+﻿// Check if user is currently in private mode.
+function isPrivateMode() {
+	var privateModeComponent = Components.classes["@mozilla.org/privatebrowsing;1"].getService(Components.interfaces.nsIPrivateBrowsingService);
+	var result = privateModeComponent.privateBrowsingEnabled;
+	return result;
+}
