fdd2ea099c6e171f6f2d33308fe065d4da665255
Riki Fridrich <riki@fczbkk.com>
2012-06-22 14:36:05 +0200

    added onSuccess and onError callbacks fixed a problem with zero-length file used word "poop" in comments... twice

--- js/file-handler.js
+++ js/file-handler.js
@@ -7,10 +7,10 @@ TODO: Error callbacks are not implemented yet.
 
 FileHandler = function (filename, directory) {
     
-    // if filename is not provided, random text file is created
+    // If filename is not provided, random text file is created.
     this.filename = filename || 'untitled' + (new Date).getTime() + '.txt';
     
-    // if directory is not provided, user's profile directory is used
+    // If directory is not provided, user's profile directory is used
     // you can find list of all special directories at:
     // https://developer.mozilla.org/en/Code_snippets/File_I%2F%2FO#Getting_special_files
     this.directory = directory || 'ProfD';
@@ -18,18 +18,42 @@ FileHandler = function (filename, directory) {
     Components.utils.import("resource://gre/modules/FileUtils.jsm");
     this.file = FileUtils.getFile(this.directory, [this.filename]);
     
+    // Create an empty file if it doesn't exist, otherwise there's a poopstorm
+    // coming when trying to read/write it.
+    if (!this.file.exists()) {
+       this.file.create(Components.interfaces.nsIFile.NORMAL_FILE_TYPE, 0777);
+    }
+
+    // Callback can either be defined as an object with onSuccess and onError,
+    // or it can be a single function (in this case it will be used to handle
+    // both error and success).
+    // If no callback is provided, an empty function will be used.
+    this.parseCallback = function (callback) {
+        var callbackFunction = (typeof callback == 'function') ? callback : function () {};
+        var callbackObject = {
+            onSuccess : callback.onSuccess || callbackFunction,
+            onError : callback.onError || callbackFunction
+        };
+        return callbackObject;
+    };
+
     // retrieves content of the file and sends it to the callback
     this.read = function (callback) {
-        var callback = callback || function () {};
+        var callback = this.parseCallback(callback);
         
         Components.utils.import("resource://gre/modules/NetUtil.jsm");
         
         NetUtil.asyncFetch(this.file, function(stream, status) {
             if (Components.isSuccessCode(status)) {
-                var data = NetUtil.readInputStreamToString(stream, stream.available());
-                callback(data);
+                // If the file has zero length, then stream.available(),
+                // then stream.avalilable() poops itself.
+                try {
+                    var data = NetUtil.readInputStreamToString(stream, stream.available());
+                } finally {
+                    callback.onSuccess(data, status);
+                }
             } else {
-                // TODO handle error
+                callback.onError(data, status);
             }
         });
     };
@@ -37,7 +61,7 @@ FileHandler = function (filename, directory) {
     // writes content to the file and calls callback when done
     this.write = function (content, callback) {
         var content = content || '';
-        var callback = callback || function () {};
+        var callback = this.parseCallback(callback);
 
         Components.utils.import("resource://gre/modules/NetUtil.jsm");
         Components.utils.import("resource://gre/modules/FileUtils.jsm");
@@ -49,9 +73,9 @@ FileHandler = function (filename, directory) {
 
         NetUtil.asyncCopy(inputStream, outputStream, function(status) {
             if (Components.isSuccessCode(status)) { 
-                callback();
+                callback.onSuccess(status);
             } else {
-                // TODO handle error
+                callback.onError(status);
             }
         });
     };
